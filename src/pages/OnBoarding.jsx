37bfa027fad77760db8d6a1209f784cad301d2b1
unknown <agrawalsujal6522@gmail.com>
2025-06-30 16:41:21 +0530

    changes for sign-in

--- src/pages/OnBoarding.jsx
+++ src/pages/OnBoarding.jsx
@@ -1,3 +1,65 @@
+// import { useUser } from "@clerk/clerk-react";
+// import { Button } from "@/components/ui/button";
+// import { useNavigate } from "react-router-dom";
+// import { useEffect } from "react";
+// import { BarLoader } from "react-spinners";
+
+// const Onboarding = () => {
+//   const { user, isLoaded } = useUser();
+//   const navigate = useNavigate();
+
+//   const navigateUser = (currRole) => {
+//     navigate(currRole === "recruiter" ? "/post-job" : "/jobs");
+//   };
+
+//   const handleRoleSelection = async (role) => {
+//     await user
+//       .update({ unsafeMetadata: { role } })
+//       .then(() => {
+//         console.log(`Role updated to: ${role}`);
+//         navigateUser(role);
+//       })
+//       .catch((err) => {
+//         console.error("Error updating role:", err);
+//       });
+//   };
+
+//   useEffect(() => {
+//     if (user?.unsafeMetadata?.role) {
+//       navigateUser(user.unsafeMetadata.role);
+//     }
+//   }, [user]);
+
+//   if (!isLoaded) {
+//     console.log("user not loaded ...");
+//     return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
+//   }
+
+//   return (
+//     <div className="flex flex-col items-center justify-center mt-40">
+//       <h2 className="gradient-title font-extrabold text-7xl sm:text-8xl tracking-tighter">
+//         I am a...
+//       </h2>
+//       <div className="mt-16 grid grid-cols-2 gap-4 w-full md:px-40">
+//         <Button
+//           variant="blue"
+//           className="h-36 text-2xl"
+//           onClick={() => handleRoleSelection("candidate")}>
+//           Candidate
+//         </Button>
+//         <Button
+//           variant="destructive"
+//           className="h-36 text-2xl"
+//           onClick={() => handleRoleSelection("recruiter")}>
+//           Recruiter
+//         </Button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Onboarding;
+
 import { useUser } from "@clerk/clerk-react";
 import { Button } from "@/components/ui/button";
 import { useNavigate } from "react-router-dom";
@@ -5,7 +67,7 @@ import { useEffect } from "react";
 import { BarLoader } from "react-spinners";
 
 const Onboarding = () => {
-  const { user, isLoaded } = useUser();
+  const { user, isLoaded, isSignedIn } = useUser();
   const navigate = useNavigate();
 
   const navigateUser = (currRole) => {
@@ -13,22 +75,25 @@ const Onboarding = () => {
   };
 
   const handleRoleSelection = async (role) => {
-    await user
-      .update({ unsafeMetadata: { role } })
-      .then(() => {
-        console.log(`Role updated to: ${role}`);
-        navigateUser(role);
-      })
-      .catch((err) => {
-        console.error("Error updating role:", err);
-      });
+    if (!user) return;
+    try {
+      await user.update({ unsafeMetadata: { role } });
+      console.log(`Role updated to: ${role}`);
+      navigateUser(role);
+    } catch (err) {
+      console.error("Error updating role:", err);
+    }
   };
 
   useEffect(() => {
-    if (user?.unsafeMetadata?.role) {
+    if (isLoaded && !isSignedIn) {
+      navigate("/?sign-in=true");
+    }
+
+    if (isLoaded && user?.unsafeMetadata?.role) {
       navigateUser(user.unsafeMetadata.role);
     }
-  }, [user]);
+  }, [user, isLoaded, isSignedIn]);
 
   if (!isLoaded) {
     console.log("user not loaded ...");
